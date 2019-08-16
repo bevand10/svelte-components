34146c0f1077d94c367a0d7aa705267ae200c4a0
Dave Bevan <dave.bevan@bbc.co.uk>
2019-08-16 10:50:48 +0100

    Create Fetch.svelte

---
+++ Fetch.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte"
+
+  export let url
+
+  let data = {}
+
+  onMount(async function() {
+    const response = await fetch(url, {
+      cache: "reload"
+    });
+    const json = await response.json();
+    data = json;
+  });
+</script>
+
+<slot {data} />
